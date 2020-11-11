129c023a011e74c3970f60a28cf6d18301c33ed8
joao paulo <joaopaulopmedeiros@gmail.com>
2020-11-11 14:09:23 -0300

    Handle text elements rendering

--- core/dom/render.js
+++ core/dom/render.js
@@ -1,5 +1,10 @@
+import { TEXT_ELEMENT } from "../constants/index";
+
 export default function render(element, container) {
-  const dom = document.createElement(element.type);
+  const dom =
+    element.type == TEXT_ELEMENT
+      ? document.createTextNode("")
+      : document.createElement(element.type);
 
   element.props.children.forEach((child) => render(child, dom));
 
