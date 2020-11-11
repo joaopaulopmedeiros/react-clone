fbb5689f005983b07508ec515f30751ae19556c2
joao paulo <joaopaulopmedeiros@gmail.com>
2020-11-11 14:05:45 -0300

    Recursively render each child

--- core/dom/render.js
+++ core/dom/render.js
@@ -1,4 +1,7 @@
 export default function render(element, container) {
   const dom = document.createElement(element.type);
+
+  element.props.children.forEach((child) => render(child, dom));
+
   container.appendChild(dom);
 }
